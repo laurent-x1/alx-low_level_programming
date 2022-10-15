799a991a2173a074ace3e45dd7f14539daa5cc11
Olatunbosun <o.oyindasola@outlook.com>
2022-10-14 21:04:35 -0700

    Task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,17 @@
-:
+#include <stdio.h>
+
+/**
+ * main - print alp in reverse
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lz;
+
+	for (lz = 'z'; lz >= 'a'; lz--)
+		putchar(lz);
+
+	putchar('\n');
+
+	return (0);
+}
