8d178feaf3148f83c72380dbaf47628415be998c
Olatunbosun <o.oyindasola@outlook.com>
2022-10-18 19:45:12 -0700

    Task 1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int -putchar(char c);
+int (-putchar(char c));
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
-int -isalpha(int c);
+int (-isalpha(int c));
 int print_sign(int n);
-int -abs(int);
+int (-abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void timestable(void);
