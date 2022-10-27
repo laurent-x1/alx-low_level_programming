a4535698db0ad3cf656bcffbe74d4fbbb7cd91f7
Olatunbosun <o.oyindasola@outlook.com>
2022-10-26 18:05:33 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,9 +12,10 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
+
 	for (i = 0; dest[i] != '\0'; i++)
 	{
-			continue;
+		continue;
 	}
 	for (j = 0; src[j] != '\0' && j < n; j++)
 	{
