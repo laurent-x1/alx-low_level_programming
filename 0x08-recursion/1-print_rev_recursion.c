2547d80a578eb122973469e54ebf8154fe17b553
Olatunbosun <o.oyindasola@outlook.com>
2022-11-02 18:47:13 -0700

    Task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,16 +2,14 @@
 
 /**
  * _print_rev_recursion - print a string in reverse
- *
  * @s: string
- *
- * Return: Always 0;
- */
+ * Return: Always 0.
+*/
 void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
-		_print_rev_recursion(s - 1);
 	}
 }
